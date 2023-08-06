484f788f0d2948243d115b994b5e426f7b90b5ca
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2023-08-06 13:10:23 +0500

    final commit

--- todo.js
+++ todo.js
@@ -1,34 +1,34 @@
 var inputValue = document.getElementById('inp');
 var newtask = document.querySelector('.taskList');
 
-function valControl(){
-    if(inputValue.value==''){
+function valControl() {
+    if (inputValue.value == '') {
         alert("Please enter some text");
     }
-    else{
+    else {
         let newelement = document.createElement('ul');
         newelement.innerHTML = inputValue.value + '<i class="fa fa-check" aria-hidden="true"></i>' + '<i class= "fa fa-trash-o" aria-hidden="true"></i> ';
         newtask.appendChild(newelement);
-        inputValue.value='';
-        
-       
-        newelement.querySelector('.fa-trash-o').addEventListener("click",remove);
-        function remove(){
+        inputValue.value = '';
+
+
+        newelement.querySelector('.fa-trash-o').addEventListener("click", remove);
+        function remove() {
             newelement.remove();
-     
-            
+
+
         }
 
-        newelement.querySelector('.fa-check').addEventListener("click",overline);
-        function overline(){
-            newelement.style.textDecoration="line-through";
-  
+        newelement.querySelector('.fa-check').addEventListener("click", overline);
+        function overline() {
+            newelement.style.textDecoration = "line-through";
+
         }
-        
-       
+
+
     }
-   
 
-   
+
+
 }
 
